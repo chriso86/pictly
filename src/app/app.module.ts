07a061766d16608e7ef9b0942645960eca86d3b8
Christmas <cnoliver86@gmail.com>
2020-02-29 17:48:44 +0200

    Worked on adding layout, components and theme to app components
    - Added home component
    - Added basic routing (Lazy-loading not needed)
    - Added basic layout
    - Implemented basic theme
    
    Notes:
    WIP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {HomeComponent} from './home/home.component';
+import {CoreModule} from './core/core.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
