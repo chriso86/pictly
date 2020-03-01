4f7f205e6c56e5ed42fb6bfd0b9517eb56801eef
Christmas <cnoliver86@gmail.com>
2020-03-02 00:16:49 +0200

    Implemented slideshow component
    Notes:
    The toggle between slides has been implemented on the slideshow model,
    however it has not been implemented on the UI

--- src/app/core/slideshow/slideshow.component.ts
+++ src/app/core/slideshow/slideshow.component.ts
@@ -1,14 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Inject, Input, OnInit} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
+import {SlideshowModel} from '../shared-models/slideshow.model';
 
 @Component({
-  selector: 'app-slideshow',
-  templateUrl: './slideshow.component.html'
+    selector: 'pictly-slideshow',
+    templateUrl: './slideshow.component.html'
 })
-export class SlideshowComponent implements OnInit {
+export class SlideshowComponent {
+    constructor(
+        public dialogRef: MatDialogRef<SlideshowComponent>,
+        @Inject(MAT_DIALOG_DATA) public data: SlideshowModel) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+    onCloseClick(): void {
+        this.dialogRef.close();
+    }
 }
