456a86447807bd19b3f60275128744a69a8ff2e3
Christmas <cnoliver86@gmail.com>
2020-03-02 00:13:31 +0200

    Updated custom material module dependencies and added styles for components
    NB!!! CONTAINS FIXES FOR BROKEN CHANGES IN PREVIOUS COMMIT
    
    - Added extra modules to the material module for global import
    - Added missing variables (oops) for the theme
    - Added search theme styles
    - Added slideshow theme styles
    
    Notes:
    Sorry if this commit breaks any previous revert.
    _colors and _box-model contain breaking changes for any reverts
    The index for components also contains a dead link (sorry again)
    Cherry pick this commit if you don't know what to do.

--- src/app/core/material.module.ts
+++ src/app/core/material.module.ts
@@ -2,18 +2,33 @@ import {NgModule} from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatSelectModule} from '@angular/material/select';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatDialogModule} from '@angular/material/dialog';
 
 @NgModule({
-  imports: [
-    MatCardModule,
-    MatFormFieldModule,
-    MatInputModule
-  ],
-  exports: [
-    MatCardModule,
-    MatFormFieldModule,
-    MatInputModule
-  ]
+    imports: [
+        MatCardModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatButtonModule,
+        MatIconModule,
+        MatSelectModule,
+        MatChipsModule,
+        MatDialogModule
+    ],
+    exports: [
+        MatCardModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatButtonModule,
+        MatIconModule,
+        MatSelectModule,
+        MatChipsModule,
+        MatDialogModule
+    ]
 })
 export class MaterialModule {
 }
